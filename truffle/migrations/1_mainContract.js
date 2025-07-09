efb14bc92c29bd8d5a1116f1cb29f116c097c891
Ushnik <ushnikbasak+github@gmail.com>
2025-07-09 06:56:56 +0530

    just_checking

--- truffle/migrations/1_mainContract.js
+++ truffle/migrations/1_mainContract.js
@@ -2,11 +2,11 @@ const MainContract = artifacts.require("./MainContract.sol");
 
 module.exports = async function (deployer, network, accounts) {
   // Choose professor and associate dean accounts
-  const professors = [accounts[1], accounts[2]];
-  const associateDeans = [accounts[3], accounts[4]];
+  const professors = [accounts[2]];
+  const associateDeans = [accounts[3]];
 
   // Log the selected accounts for clarity
-  console.log("Deploying mainContract from:", accounts[0]);
+  console.log("Deploying mainContract from:", accounts[1]);
   console.log("Professors:", professors);
   console.log("Associate Deans:", associateDeans);
 
